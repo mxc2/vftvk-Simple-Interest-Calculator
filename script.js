87eb4d00f1c08f3ccc77709ab9a548cae182ddab
Marcus-indrek Simmer <70900223+mxc2@users.noreply.github.com>
2022-09-18 18:32:14 +0000

    Quick fix
    Fixed couple of missing commas.

--- script.js
+++ script.js
@@ -10,9 +10,7 @@ function compute()
         var interest = principal * years * rate /100;   // Calculated interest
         var year = new Date().getFullYear()+parseInt(years);    // When the user gets it's money back
     
-    
-
-        result.innerHTML = "If you deposit " + "<span class='highlited-text'>" + principal + "</span>,<br/>at an interest rate of <span class='highlited-text'>" + rate + "%</span>.<br/>You will recieve an amount of <span class='highlited-text'>" + interest + "</span>.<br/>in the year <span class='highlited-text'>" + year + "</span><br/><br/>";
+        result.innerHTML = "If you deposit " + "<span class='highlited-text'>" + principal + "</span>,<br/>at an interest rate of <span class='highlited-text'>" + rate + "%</span>.<br/>You will recieve an amount of <span class='highlited-text'>" + interest + "</span>,<br/>in the year <span class='highlited-text'>" + year + "</span><br/><br/>";
     }else{
         alert("Enter a positive number");
         document.getElementById("principal").focus();
